1bfc85899d6399326b5bac5856824e1fff9877fe
Miguel Tovar Amaral. <miguel.tovar.amaral@gmail.com>
2026-08-13 10:27:58 -0500

    wire profile hub persistence

--- apps/employx_flutter/lib/core/di/service_locator.dart
+++ apps/employx_flutter/lib/core/di/service_locator.dart
@@ -1,29 +1,58 @@
 import 'package:get_it/get_it.dart';
+
 import '../api/api_client.dart';
-import '../repositories/mission_repository.dart';
-import '../repositories/command_repository.dart';
-import '../actions/mission_actions.dart';
 import '../actions/command_actions.dart';
+import '../actions/mission_actions.dart';
 import '../actions/system_actions.dart';
-import '../repositories/cv_repository.dart';
 import '../providers/mission_provider.dart';
+import '../providers/profile_hub_provider.dart';
 import '../providers/upload_provider.dart';
+import '../repositories/command_repository.dart';
+import '../repositories/cv_repository.dart';
+import '../repositories/mission_repository.dart';
+import '../repositories/profile_repository.dart';
 
 final sl = GetIt.instance;
 
 void setupServiceLocator() {
-  // Core
   sl.registerLazySingleton<ApiClient>(() => ApiClient());
 
-  // Repositories
-  sl.registerLazySingleton<MissionRepository>(() => MissionRepository(sl()));
-  sl.registerLazySingleton<CommandRepository>(() => CommandRepository(sl()));
-  sl.registerLazySingleton<CVRepository>(() => CVRepository());
+  sl.registerLazySingleton<MissionRepository>(
+    () => MissionRepository(sl()),
+  );
+  sl.registerLazySingleton<CommandRepository>(
+    () => CommandRepository(sl()),
+  );
+  sl.registerLazySingleton<CVRepository>(
+    () => CVRepository(),
+  );
+  sl.registerLazySingleton<ProfileRepository>(
+    () => ProfileRepository(),
+  );
 
-  // Actions & Providers
-  sl.registerLazySingleton<MissionActions>(() => MissionActions(sl<MissionRepository>()));
-  sl.registerLazySingleton<CommandActions>(() => CommandActions(sl()));
-  sl.registerLazySingleton<SystemActions>(() => SystemActions());
-  sl.registerLazySingleton<MissionProvider>(() => MissionProvider());
-  sl.registerLazySingleton<UploadProvider>(() => UploadProvider());
+  sl.registerLazySingleton<MissionActions>(
+    () => MissionActions(
+      sl<MissionRepository>(),
+      cvRepository: sl<CVRepository>(),
+      profileRepository: sl<ProfileRepository>(),
+    ),
+  );
+  sl.registerLazySingleton<CommandActions>(
+    () => CommandActions(sl()),
+  );
+  sl.registerLazySingleton<SystemActions>(
+    () => SystemActions(),
+  );
+  sl.registerLazySingleton<MissionProvider>(
+    () => MissionProvider(),
+  );
+  sl.registerLazySingleton<UploadProvider>(
+    () => UploadProvider(),
+  );
+  sl.registerLazySingleton<ProfileHubProvider>(
+    () => ProfileHubProvider(
+      sl<MissionRepository>(),
+      sl<ProfileRepository>(),
+    ),
+  );
 }
